5a33af7a18fb64ebfc70c5b1c2ca4dbfdfcf4b8c
trang051201 <trangtrau201@gmail.com>
2022-04-26 23:17:04 +0700

    upload

---
+++ src/Bai3_2/Main.java
@@ -0,0 +1,15 @@
+package Bai3_2;
+
+public class Main {
+
+	public static void main(String[] args) {
+     TamGiac tg=new TamGiac();
+     tg.KiemTra();
+     tg.TinhChuVi();
+     tg.TinhDienTich();
+     DuongTron Dt=new DuongTron();
+     Dt.InThongTin();
+	}
+	//Lê Hữu Văn 2021050719
+
+}
